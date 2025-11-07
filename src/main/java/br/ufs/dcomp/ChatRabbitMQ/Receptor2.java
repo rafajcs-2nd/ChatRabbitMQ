3fc8d7ad76f7998bc526f0dba35f486eb98384ff
Ubuntu <ubuntu@ip-172-31-4-113.ec2.internal>
2025-11-07 00:53:01 +0000

    emissor e receptor2

---
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Receptor2.java
@@ -0,0 +1,33 @@
+package br.ufs.dcomp.ChatRabbitMQ;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+
+public class Receptor {
+
+ private static final String EXCHANGE_NAME = "usuarios_direct";
+ private final Connection connection;
+ 
+ public Receptor(Connection connection){
+     this.connection = connection;
+ }
+ 
+ public void comunicacaoON(String username) throws IOException{
+    Channel channel = connection.createChannel();
+    channel.queueDeclare(username, false, false, true, null);
+    channel.queueBind(username, EXCHANGE_NAME, username);
+    
+    Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String fullMessage = new String(body, "UTF-8");
+                
+    }
+    
+    
+ };
+ channel.basicConsume(username, true, consumer);
+ 
+}
+}
